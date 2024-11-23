5adcab700424bbfd46031155eb2fb0477d568301
Youssef-olb10 <youssef.ya2003@gmail.com>
2024-11-23 16:59:34 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -14,7 +14,7 @@ class Connection {
     }
 
     function createDatabase($dbName) {
-        // Crée la base de données si elle n'existe pas
+        
         $sql = "CREATE DATABASE IF NOT EXISTS $dbName";
         if (mysqli_query($this->connexion, $sql)) {
             echo "Database '$dbName' created successfully.<br>";
@@ -24,14 +24,14 @@ class Connection {
     }
 
     function selectDatabase($dbName) {
-        // Sélectionne la base de données
+
         if (!mysqli_select_db($this->connexion, $dbName)) {
             echo "Error selecting database: " . mysqli_error($this->connexion) . "<br>";
         }
     }
 
     function createTable($query) {
-        // Crée la table Clients si elle n'existe pas
+        
         if (mysqli_query($this->connexion, $query)) {
             echo "Table Clients created successfully<br>";
         } else {
